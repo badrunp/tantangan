f8ea263d696e2f22e3fbda546f06ee67b75e525e
= <bbadrunn@gmail.com>
2022-02-18 17:58:01 +0700

    menyelesaikan project menghitung huruf menggunakan python

---
+++ menghitung_huruf.py
@@ -0,0 +1,8 @@
+def char_counter(str, f):
+    print(str.count(f))
+    return str.count(f)
+
+
+char_counter("Aku bukan pemalas. Aku sedang menjalankan mode hemat energi.", "e") # 7
+char_counter("Anda sopan kami curiga.", "a") # 4
+char_counter("Hidup itu sederhana. Goreng, angkat, lalu tiriskan.", "i") # 4
