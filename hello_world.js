e3779ef14ee67d291b93711c70381e601c663c0c
= <bbadrunn@gmail.com>
2022-02-19 13:48:32 +0700

    menyelesainkan tantangan hello world menggunakan javascript

--- hello_world.js
+++ hello_world.js
@@ -1,3 +1,10 @@
 console.log("Hello Javascript!")
 console.log("Apa kabar dunia?")
 console.log("Saya ingin belajar Javascript.")
+
+/*
+Buatlah program diatas menghasilkan output berikut:
+- Hello PHP!
+- Bukan Pemberi Harapan Palsu!
+- Saya ingin belajar PHP.
+*/
