fd866a2c3a71bbf997d7d5e8aa1e522e1a68980f
= <bbadrunn@gmail.com>
2022-02-19 13:52:59 +0700

    menyelesaikan tantangan menghitung huruf menggunakan python

--- menghitung_huruf.py
+++ menghitung_huruf.py
@@ -1,8 +1,10 @@
 def char_counter(str, f):
-    print(str.count(f))
     return str.count(f)
 
+print(char_counter("Aku bukan pemalas. Aku sedang menjalankan mode hemat energi.", "e")) # 7
+print(char_counter("Anda sopan kami curiga.", "a")) # 4
+print(char_counter("Hidup itu sederhana. Goreng, angkat, lalu tiriskan.", "i")) # 4
 
-char_counter("Aku bukan pemalas. Aku sedang menjalankan mode hemat energi.", "e") # 7
-char_counter("Anda sopan kami curiga.", "a") # 4
-char_counter("Hidup itu sederhana. Goreng, angkat, lalu tiriskan.", "i") # 4
+"""
+Buatlah sebuah fungsi char_counter dimana menerima 2 buah parameter, parameter pertama adalah sebuah kalimat dan yang kedua adalah sebuah huruf (character) yang akan dihitung jumlahnya
+"""
