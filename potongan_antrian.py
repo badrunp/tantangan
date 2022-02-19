243d4c32b4269a6d718ebe3222779484a448a3ee
= <bbadrunn@gmail.com>
2022-02-19 14:01:20 +0700

    menyelesaikan tantangan pemotong antrian menggunakan python

--- potongan_antrian.py
+++ potongan_antrian.py
@@ -8,9 +8,14 @@ def potong_antrian(antrian, pengacau):
     
     return antrian
 
+print(potong_antrian([1,2,3,4], [6,7,8])) # [1,2,6,7,8,3,4]
+print(potong_antrian([[1,2],[3,4]], [[11,12],[13,14,15]])) # [[1,2],[11,12],[13,14,15],[3,4]]
 
-potongan1 = potong_antrian([1,2,3,4], [6,7,8]) # [1,2,6,7,8,3,4]
-potongan2 = potong_antrian([[1,2],[3,4]], [[11,12],[13,14,15]]) # [[1,2],[11,12],[13,14,15],[3,4]]
-
-print(potongan1)
-print(potongan2)
+"""
+Lengkapi fungsi potong_antrian diatas untuk mensimulasikan kejadian tersebut dimana fungsi tersebut menerima 2 buah parameter:
+- Parameter pertama: sebuah list genap (list atau list-dalam-list)
+- Parameter kedua: sebuah list dimana valuenya akan disisipkan ke list pertama.
+Contoh:
+- potong_antrian([1,2,3,4], [10,11]) # output: [1, 2, 10, 11, 3, 4]
+- potong_antrian([[1,2], [3,4]], [[10,11], [12,13]]) # output: ([[1,2], [10,11], [12,13], [3,4]]
+"""
