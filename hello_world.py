19a9ef275e9e3ec03b942aff7b8a9c3148e3f8cc
= <bbadrunn@gmail.com>
2022-02-19 13:47:02 +0700

    menyelesainkan tantangan hello world menggunakan python

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,11 @@
 
-print("Hello PHP!");
-print("Bukan Pemberi Harapan Palsu!");
-print("Saya ingin belajar PHP.");
+print("Hello PHP!")
+print("Bukan Pemberi Harapan Palsu!")
+print("Saya ingin belajar PHP.")
+
+"""
+Buatlah program diatas menghasilkan output berikut:
+- Hello PHP!
+- Bukan Pemberi Harapan Palsu!
+- Saya ingin belajar PHP.
+"""
