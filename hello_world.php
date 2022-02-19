ae83b2dc77f6eaa9f909bf4c0a026f4ce0a397f1
= <bbadrunn@gmail.com>
2022-02-19 13:47:46 +0700

    menyelesainkan tantangan hello world menggunakan php

--- hello_world.php
+++ hello_world.php
@@ -2,3 +2,10 @@
 echo "Hello PHP!\n";
 echo "Bukan Pemberi Harapan Palsu!\n";
 echo "Saya ingin belajar PHP.\n";
+
+/*
+Buatlah program diatas menghasilkan output berikut:
+- Hello PHP!
+- Bukan Pemberi Harapan Palsu!
+- Saya ingin belajar PHP.
+*/
